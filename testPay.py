7f0e32c96bb035295b8e5f5ba67183471e50f377
Marco <sz547073165@qq.com>
2017-11-13 02:31:14 +0800

    五日均线法，检查频率调高15s一次，添加条件，上升趋势时，市价>买入成交价*1.01才能卖出 	添加均线斜率法，ma5和ma11的斜率为正的时候，为买入信号，ma5为负的时候为卖出信号

--- testPay.py
+++ testPay.py
@@ -21,28 +21,14 @@ client = huoBiApi.ApiClient(API_KEY, API_SECRET)
 accs = client.get('/v1/account/accounts')
 accountIdValue = accs[0].id
 
-#创建并执行一个新订单，返回订单ID
-def place(amount,typeValue):
-    params={}
-    params['account-id']=accountIdValue
-    params['amount']=amount#限价单表示下单数量，市价买单时表示买多少钱，市价卖单时表示卖多少币
-    params['symbol']=symbolValue
-    params['type']=typeValue
-    print(params)
-    return client.post('/v1/order/orders/place',params)
+#查询最后一个买入订单的成交均价*1.005的价格
+def getLastBuyOrderPrice():
+    matchResults=getMatchResults()
+    for match in matchResults:
+        if match['type']=='buy-market' and match['symbol']==symbolValue:
+            print(match)
+            return round(float(match['price']) * 1.01,4)
 
-def getBlance(currency):
-    subaccs = client.get('/v1/account/accounts/%s/balance' % accountIdValue)
-    for sub in subaccs['list']:
-        if sub['currency'] == currency and sub['type'] == 'trade':
-            return sub['balance']
+print(getLastBuyOrderPrice())
 
-#
 
-balance='143.2222222222'
-pointIndex=balance.index('.')
-mmm=float(balance[0:5+pointIndex])
-print(balance.__class__)
-print(pointIndex)
-print(balance[0:5+pointIndex])
-print(mmm)
